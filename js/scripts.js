23173254258a11637374fa433504e77fa9dfe3d1
Daniel Bennett and Rohan Moore <rohan@>
2018-11-01 08:32:29 -0700

    move everything to functions and objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,85 @@
-const ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+const RANKS = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
 
-const suits = ["clubs", "diamonds", "hearts", "spades"];
+const SUITS = ["clubs", "diamonds", "hearts", "spades"];
 
-const deckSize = 52;
+const DECK_SIZE = 52;
+
+function Deck() {
+  this.cards = makeCards();
+}
 
 function Card(name, imageSrc) {
   this.name = name;
   this.imageSrc = imageSrc;
 }
 
-var getRandomCard = function(deck) {
-  return deck.splice(Math.floor(Math.random()*deck.length), 1)[0];
+function Hand(owner) {
+  this.hand = [];
+  this.owner = owner;
 }
 
-var returnToDeck = function(deck, card) {
-  deck.push(card);
+function Game(playerOneName) {
+  this.playerHand = new Hand(playerOneName);
+  this.AIHand = new Hand("House");
+  this.deck = new Deck();
 }
+
+Game.prototype.deal = function() {
+
+}
+
+Hand.prototype.addCard = function(card) {
+  (this.hand).push(card);
+}
+
+Hand.prototype.clearHand = function() {
+  return (this.hand).splice(0, (this.hand).length);
+}
+
+Hand.prototype.displayHand = function() {
+  (this.hand).forEach(function(card) {
+    $("#card-list").append("<li>" + card.name + "</li>")
+    showImage(card.imageSrc);
+  });
+}
+
+Deck.prototype.getRandomCard = function() {
+   return (this.cards).splice(Math.floor(Math.random()*52), 1)[0];
+}
+
+Deck.prototype.returnToDeck = function(cards) {
+  cards.forEach(function(card) {
+    (this.cards).push(card);
+  });
+}
+
 var showImage = function(src) {
   var img = document.createElement("img");
   img.src = src;
   img.width = 200;
   img.height = 300;
   img.alt = "card";
-  document.body.appendChild(img);
+  $("#cardImages").append(img);
 }
 
-$(document).ready(function() {
-  var deck = [];
+function makeCards() {
   var cardNames = [];
   var images = [];
-  for (i = 1; i <= 52; i++) {
+  var output = [];
+  for (i = 1; i <= DECK_SIZE; i++) {
     images.push("img/card" + i + ".png");
   }
-  suits.forEach(function(suit) {
-    ranks.forEach(function(rank) {
+  SUITS.forEach(function(suit) {
+    RANKS.forEach(function(rank) {
       cardNames.push(rank + " of " + suit);
     });
   });
-  for (i = 0; i < 52; i++) {
-    deck.push(new Card(cardNames[i], images[i]));
+  for(i = 0; i < DECK_SIZE; i++) {
+    output.push(new Card(cardNames[i], images[i]))
   }
-  // showImage("img/test.jpg");
-  $("#deal").click(function() {
-    var hand = [];
-    for (i = 0; i < 7; i++) {
-      hand.push(getRandomCard(deck));
-    }
-    hand.forEach(function(card) {
-      $("#card-list").append("<li>" + card.name + "</li>")
-      showImage(card.imageSrc)
-      returnToDeck(deck, card);
-    });
-  });
+  return output;
+}
+
+$(document).ready(function() {
+
 });
